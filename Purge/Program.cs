6c986d724a66f15314ba9d50d46c7051fe6dd009
liuwei <liuwei@zenchn.com>
2018-03-02 10:04:57 +0800

    add parallel purge

--- Purge/Program.cs
+++ Purge/Program.cs
@@ -34,37 +34,49 @@ namespace Receive
         {
             try
             {
-                List<IConnection> connections = GetRabbitMqConnection();
-                foreach (var connection in connections)
-                {
-                    var channel = connection.CreateModel();               
-                    var GPSQueueName = mqSec["GPSQueue"];//消息队列名
-                    int GPSQueueCount = int.Parse(mqSec["GPSQueueCount"]);
-                    var alarmQueueName = mqSec["AlarmQueue"];
-                    int alarmQueueCount = int.Parse(mqSec["AlarmQueueCount"]);
-                    for (int i = 1; i <= GPSQueueCount; i++)
-                    {
-                        string qName = GPSQueueName + "_" + i;
-                        channel.QueuePurge(qName);
-                        
-                        Console.WriteLine("{0} purged!", qName);
-                    }
-
-                    for (int i = 1; i <= alarmQueueCount; i++)
-                    {
-                        string qName = alarmQueueName + "_" + i;
-                        channel.QueuePurge(qName);
-                        Console.WriteLine("{0} purged!", qName);
-                    }
-                }
-                
-
+                PurgeQueue();
             }
             catch (Exception e)
             {
                 logger.Error(e.GetType() + ": " + e.Message);
             }
 
+        }
+
+        private static void PurgeQueue()
+        {
+            //从App.config获取RabbiMq配置参数
+            String hostNames = mqSec["HostName"];
+            String[] hostArr = hostNames.Split(',');
+            for(int index = 0;index < hostArr.Length;index++)
+            {
+                factory.HostName = hostArr[index];
+                factory.UserName = mqSec["UserName"];//用户名
+                factory.Password = mqSec["Password"];//密码
+                factory.Port = int.Parse(mqSec["Port"]);//端口
+
+                factory.AutomaticRecoveryEnabled = true;//允许自动恢复
+                factory.NetworkRecoveryInterval = TimeSpan.FromSeconds(10);//网络恢复间隔
+                factory.RequestedHeartbeat = 30;//心跳
+                int gpsCount = int.Parse(mqSec["GPSQueueCount"]);
+                int alarmCount =  int.Parse(mqSec["AlarmQueueCount"]);
+                var GPSQueueName = mqSec["GPSQueue"];//消息队列名
+                var alarmQueueName = mqSec["AlarmQueue"];
+                Parallel.For(1,gpsCount+1,i => {
+                    var connection = factory.CreateConnection();
+                    var channel = connection.CreateModel();
+                    string qName = GPSQueueName + "_" + i;
+                    channel.QueuePurge(qName);
+                    Console.WriteLine("{0}<<{1}>> purged!", qName,hostArr[index]);
+                });
+                Parallel.For(1,alarmCount+1,i => {
+                    var connection = factory.CreateConnection();
+                    var channel = connection.CreateModel();
+                    string qName = alarmQueueName + "_" + i;
+                    channel.QueuePurge(qName);
+                    Console.WriteLine("{0}<<{1}>> purged!", qName,hostArr[index]);
+                });
+            }
         }
 
         /// <summary>
